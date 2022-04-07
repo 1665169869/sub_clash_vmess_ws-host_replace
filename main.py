bea695e6215cdf719af39d951b7c30534e6840d7
baiyu16 <1665169869@qq.com>
2022-04-07 12:23:29 +0800

    添加了trojan和非ws协议的支持

--- main.py
+++ main.py
@@ -28,15 +28,30 @@ def index():
 def sub():
     sub_url = request.args.get("sub_url")
     replace_host = request.args.get("host")
-
+    if sub_url == None or replace_host == None:
+        return ""
     clash_config = _get(sub_url)
     if clash_config == None:
         abort(404)
     
-    datas = yaml.load(clash_config, Loader=yaml.FullLoader)
+    datas: dict = yaml.load(clash_config, Loader=yaml.FullLoader)
+    if datas.get("proxies") == None:
+        return ""
+    for index in range(len(datas['proxies'])):
+        clash_type = datas['proxies'][index].get('type')
+        if clash_type == "vmess": # vmess协议处理
+            network = datas['proxies'][index].get("network")
+            if network == "ws": # ws的处理
+                if datas['proxies'][index].get("ws-headers") is None: 
+                    datas["proxies"][index]["ws-headers"] = {}
+                datas["proxies"][index]["ws-headers"]["Host"] = replace_host
+            else:
+                datas['proxies'][index]['tls-hostname'] = replace_host
+        if clash_type == "trojan": # trojan协议处理
+            datas['proxies'][index]["sni"] = replace_host
+        
+            
 
-    for index in range(len(datas["proxies"])):
-        datas["proxies"][index]["ws-headers"]["Host"] = replace_host
     datas = yaml.dump(datas, allow_unicode=True)
     return datas
 
